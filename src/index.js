f60ea6be1e1a9f90b3a74efaf822dc7cbaf446be
R.Luck <rbxlk@protonmail.com>
2022-10-31 13:26:50 +0000

    Add basic components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Hello from "./Hello";
+import Gallery from "./Gallery";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Hello />);
+root.render(
+    <div>
+        <Gallery/>
+    </div>
+);
