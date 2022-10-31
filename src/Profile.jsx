3aa5c7a885a295426e482d5bfc00f6fbd4710ff7
R.Luck <rbxlk@protonmail.com>
2022-10-31 14:22:53 +0000

    Restructure components into App file

---
+++ src/Profile.jsx
@@ -0,0 +1,16 @@
+export function Profile() {
+    return (
+        <>
+            <img 
+            src="https://i.imgur.com/yXOvdOSs.jpg" 
+            alt="Hedy Lamarr" 
+            className="photo"
+            />
+            <ul>
+            <li>Invent new traffic lights</li>
+            <li>Rehearse a movie scene</li>
+            <li>Improve the spectrum technology</li>
+            </ul>
+        </>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Gallery from "./Gallery";
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-    <div>
-        <Gallery/>
-    </div>
+    <App/>
 );
