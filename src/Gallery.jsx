bec43e23d92adb86cc5a15a4047c81288e016657
R.Luck <rbxlk@protonmail.com>
2022-11-01 10:27:10 +0000

    Implement use of props and children

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,9 +4,26 @@ export default function Gallery() {
     return (
       <section>
         <h1>Amazing scientists</h1>
-        <Profile />
-        <Profile />
-        <Profile />
+        <Profile 
+          person={{ 
+            name: 'Lin Lanying', 
+            imageId: '1bX5QH6'}}
+          size={100}
+        />
+        <Profile 
+          size={100}
+          person={{
+            name: 'Aklilu Lemma', 
+            imageId: 'OKS67lh'
+          }}
+        />
+        <Profile 
+          size={100}
+          person={{ 
+            name: 'Katsuko Saruhashi', 
+            imageId: 'YfeOqp2'
+          }}
+        />
       </section>
     );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,16 +1,16 @@
-export function Profile() {
+import { Avatar } from "./Avatar";
+
+function Card({ children}) {
     return (
-        <>
-            <img 
-            src="https://i.imgur.com/yXOvdOSs.jpg" 
-            alt="Hedy Lamarr" 
-            className="photo"
-            />
-            <ul>
-            <li>Invent new traffic lights</li>
-            <li>Rehearse a movie scene</li>
-            <li>Improve the spectrum technology</li>
-            </ul>
-        </>
+        <div className="card">
+            {children}
+        </div>
+    )
+}
+export function Profile(props) {
+    return (
+        <Card>
+            <Avatar {...props} />
+        </Card>
     );
   }
