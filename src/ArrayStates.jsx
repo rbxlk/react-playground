9b774e2a67508c851f07b986680d8ed08ca6a2e6
R.Luck <rbxlk@protonmail.com>
2022-11-14 11:04:30 +0000

    Add ShapeEditor to practice replacing items in an array

--- src/ArrayStates.jsx
+++ src/ArrayStates.jsx
@@ -42,3 +42,55 @@ export default function AddRemoveArray(){
         </>
     )
 }
+
+export function ShapeEditor() {
+    let initialShapes = [
+        { id: 0, type: 'circle', x: 50, y: 100 },
+        { id: 1, type: 'square', x: 150, y: 100 },
+        { id: 2, type: 'circle', x: 250, y: 100}
+    ];
+
+    const [shapes, setShapes] = useState(
+        initialShapes
+    );
+
+    function handleClick(){
+        const nextShapes = shapes.map(shape => {
+            if (shape.type === 'square') {
+                // No change
+                return shape;
+            } else {
+                return {
+                    ...shape,
+                    y: shape.y + 50
+                };
+            }
+        });
+        setShapes(nextShapes)
+    }
+
+    return (
+        <>
+            <Button onClick={handleClick}>
+                Move circles down!
+            </Button>
+            {shapes.map(shape => (
+                <div
+                    key={shape.id}
+                    style={{
+                        background: 'purple',
+                        position: 'absolute',
+                        left: shape.x,
+                        top: shape.y,
+                        borderRadius:
+                            shape.type === 'circle'
+                            ? '50%' : '',
+                        width: 20,
+                        height: 20
+                    }}
+                >
+                </div>
+            ))}
+        </>
+    )
+}
