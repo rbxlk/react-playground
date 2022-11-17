1a7c3604bb089163885c0cf685a5fad3aaed49d9
R.Luck <rbxlk@protonmail.com>
2022-11-17 13:13:21 +0000

    Remove unrequired variable from SideBarLinks

--- src/SideBarLinks.jsx
+++ src/SideBarLinks.jsx
@@ -4,18 +4,17 @@ import { Link } from "react-router-dom";
 import { links } from "./data"
 
 export default function SideBarLinks(){
-    const linkList = links.map(link =>
-        <Fragment key={link.id}>
-            <Text
-                component={Link}
-                variant="link"
-                to={link.linkPath}
-            >
-                {link.title}
-            </Text>
-        </Fragment>
-    )
     return(
-        <>{linkList}</>
+        links.map(link =>
+            <Fragment key={link.id}>
+                <Text
+                    component={Link}
+                    variant="link"
+                    to={link.linkPath}
+                >
+                    {link.title}
+                </Text>
+            </Fragment>
+        )
     );
 }
